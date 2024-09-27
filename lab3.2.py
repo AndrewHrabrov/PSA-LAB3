19669669b3ea958807712158e33bdf71cf3ab9cb
AndrewHrabrov <medveddimon623@gmail.com>
2024-09-27 12:58:43 +0300

    Task 2

---
+++ lab3.2.py
@@ -0,0 +1,7 @@
+with open('/home/stud/Desktop/PSA/lab3/mat_tech_baza.desktop', 'w', encoding='utf-8') as f:
+    f.write('[Desktop Entry]' + '\n'
+            'Encoding=UTF-8' + '\n'
+            'Name=mat tech baza' + '\n'
+            'Type=Link' + '\n'
+            'URL=https://dep_ibst.pnzgu.ru/umd/mat_teh_baza' + '\n'
+            'Icon=text-html')
